c875cd68522801bab2d4206aed4de003f247b981
Benavente <lbenavente@proton.me>
2023-12-04 00:00:31 +0100

    base commit

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -1,27 +1,31 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
+pragma solidity ^0.8.17;
 
 contract Escrow {
-	address public arbiter;
-	address public beneficiary;
-	address public depositor;
+    address public depositor;
+    address public beneficiary;
+    address public arbiter;
 
-	bool public isApproved;
+    bool public isApproved;
 
-	constructor(address _arbiter, address _beneficiary) payable {
-		arbiter = _arbiter;
-		beneficiary = _beneficiary;
-		depositor = msg.sender;
-	}
+    event Approved(uint _balance);
 
-	event Approved(uint);
+    constructor(address _arbiter, address _beneficiary) payable {
+        depositor = msg.sender;
+        beneficiary = _beneficiary;
+        arbiter = _arbiter;
+    }
 
-	function approve() external {
-		require(msg.sender == arbiter);
-		uint balance = address(this).balance;
-		(bool sent, ) = payable(beneficiary).call{value: balance}("");
- 		require(sent, "Failed to send Ether");
-		emit Approved(balance);
-		isApproved = true;
-	}
+    function approve() external {
+        require(msg.sender == arbiter);
+
+        uint balance = address(this).balance;
+        
+        (bool success, ) = beneficiary.call{value: balance}("");
+        require(success, "Failed to send Ether");
+
+        isApproved = true;
+
+        emit Approved(balance);
+    }
 }
